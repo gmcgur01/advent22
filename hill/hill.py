d08ce87477ba0483d2c0b6b8d59b58003b761589
gmcgur01 <George.McGurkin@tufts.edu>
2022-12-13 13:02:10 -0500

    hill part 2

--- hill/hill.py
+++ hill/hill.py
@@ -9,6 +9,7 @@ def main():
         sys.exit(f"Usage: {sys.argv[0]} <file name>")
 
     grid = []
+    starts = []
 
     try:
         with open(sys.argv[1]) as file:
@@ -17,13 +18,55 @@ def main():
                 row = []
                 for i in range(len(line)):
                     new_pos = Position(line[i], len(grid), i, False)
-                    if new_pos.value == "S":
-                        start = new_pos
+                    if new_pos.value == "S" or new_pos.value == "a":
+                        starts.append(new_pos)
                     row.append(new_pos)
                 grid.append(row)
     except FileNotFoundError:
         sys.exit(f"{sys.argv[1]}: Unable to open file")
 
+    paths = []
+    for start in starts:
+        reset_board(grid)
+        path = get_path(start, grid)
+        if path != -1:
+            paths.append(path)
+    print(min(paths))
+
+
+@dataclass
+class Position:
+    value: str
+    row_num: int 
+    col_num: int
+    visited: bool
+
+    def get_height(self):
+        if self.value == "S":
+            return 0
+        if self.value == "E":
+            return 26
+        return ord(self.value) - 97
+
+@dataclass
+class BFS_Node:
+    pos: Position
+    path_length: int
+
+def can_visit(grid, curr_pos, row_num, col_num):
+    if row_num < 0 or row_num >= len(grid) or col_num < 0 or col_num >= len(grid[0]):
+        return False
+    if grid[row_num][col_num].visited:
+        return False
+    if grid[row_num][col_num].get_height() - curr_pos.get_height() <= 1:
+        return True
+
+def reset_board(grid):
+    for row in grid:
+        for position in row:
+            position.visited = False
+
+def get_path(start, grid):
     queue = deque()
 
     queue.append(BFS_Node(start, 0))
@@ -31,8 +74,7 @@ def main():
     while len(queue) != 0:
         curr_node = queue[0]
         if curr_node.pos.value == "E":
-            print (curr_node.path_length)
-            break
+            return curr_node.path_length
         # check up
         if can_visit(grid, curr_node.pos, curr_node.pos.row_num - 1, curr_node.pos.col_num):
             grid[curr_node.pos.row_num - 1][curr_node.pos.col_num].visited = True
@@ -51,32 +93,7 @@ def main():
             queue.append(BFS_Node(grid[curr_node.pos.row_num][curr_node.pos.col_num + 1], curr_node.path_length + 1))
         queue.popleft()
 
-@dataclass
-class Position:
-    value: str
-    row_num: int 
-    col_num: int
-    visited: bool
-
-    def get_height(self):
-        if self.value == "S":
-            return -1
-        if self.value == "E":
-            return 26
-        return ord(self.value) - 97
-
-@dataclass
-class BFS_Node:
-    pos: Position
-    path_length: int
-
-def can_visit(grid, curr_pos, row_num, col_num):
-    if row_num < 0 or row_num >= len(grid) or col_num < 0 or col_num >= len(grid[0]):
-        return False
-    if grid[row_num][col_num].visited:
-        return False
-    if grid[row_num][col_num].get_height() - curr_pos.get_height() <= 1:
-        return True
+    return -1
 
 if __name__ == "__main__":
     main()
