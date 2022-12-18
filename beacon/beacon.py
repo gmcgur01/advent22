b56b5042fdd85ba9fea974da7b8913255f0bdd85
gmcgur01 <George.McGurkin@tufts.edu>
2022-12-18 01:01:45 -0500

    beacon part 2

--- beacon/beacon.py
+++ beacon/beacon.py
@@ -13,105 +13,61 @@ def main():
         with open(sys.argv[1]) as file:
             for line in file:
                 line = line.strip()
-                sensor, beacon = parse_line(line)
-                sensors.append(Sensor(sensor, beacon))
+                sensor = parse_line(line)
+                sensors.append(sensor)
 
     except FileNotFoundError:
         sys.exit(f"{sys.argv[1]}: Unable to open file")
 
-
+    
     for i in range(4000001):
-        print(i)
-        for j in range(4000001):
-            in_range = False
-            for sensor in sensors:
-                if sensor.in_range(j, i):
-                    in_range = True
-                    break
-            if not in_range:
-                print(j * 4000000 + i)=
+        ranges = []
+        for sensor in sensors:
+            interval = sensor.get_range(i)
+            if interval != None: ranges.append(interval)
+        ranges.sort(key=get_start)
+        farthest_right = 0
+        for j in range(len(ranges) - 1):
+            if ranges[j][1] > farthest_right: 
+                farthest_right = ranges[j][1]
+            if farthest_right - ranges[j+1][0] < -1:
+                print((farthest_right + 1) * 4000000 + i)
+                return
+
+
 
 def parse_line(line):
     if matches := re.search(r"Sensor at x=(-*\d+), y=(-*\d+): closest beacon is at x=(-*\d+), y=(-*\d+)", line):
-        sensor = Position(int(matches.group(1)), int(matches.group(2)))
-        beacon = Position(int(matches.group(3)), int(matches.group(4)))
-        return sensor, beacon
-
-@dataclass
-class Position:
-    x: int
-    y: int
+        sensor_x = int(matches.group(1))
+        sensor_y = int(matches.group(2))
+        beacon_x = int(matches.group(3))
+        beacon_y = int(matches.group(4))
+        range = abs(beacon_x - sensor_x) + abs(beacon_y - sensor_y)
+        return Sensor(sensor_x, sensor_y, range)
 
 class Sensor:
-    def __init__(self, sensor, beacon):
-        self.sensor = sensor
-        self.beacon = beacon
-        self.distance = abs(beacon.x - sensor.x) + abs(beacon.y - sensor.y)
+    def __init__(self, sensor_x, sensor_y, range):
+        self.x = sensor_x
+        self.y = sensor_y
+        self.range = range
     
     def __str__(self):
-        return "(" + str(self.sensor) + ", " + str(self.beacon) + ", " + str(self.distance) + ")"
+        return "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.range) + ")"
+
+    def get_range(self, y):
+        y_diff = abs(y - self.y)
+        if y_diff > self.range:
+            return None
+        return (self.x - (self.range - y_diff)), (self.x + (self.range - y_diff))
 
     def in_range(self, x, y):
-        return abs(self.sensor.x - x) + abs(self.sensor.y - y) <= self.distance
+        return abs(self.x - x) + abs(self.y - y) <= self.range
 
-def far_left(sensor):
-    return sensor.sensor.x - sensor.distance
+def get_start(range):
+    return range[0]
 
-def far_right(sensor):
-    return sensor.sensor.x + sensor.distance
-
-class Interval:
-    def __init__(self, start=None, end=None):
-        
-        self.start = start
-        self.end = end
-        self.left_node = None
-        self.right_node = None
-
-    def add_sensor(self, sensor, y_pos):
-        range = sensor.distance - abs(y_pos - sensor.sensor.y)
-        if range < 0:
-            return
-
-        start = sensor.sensor.x - range
-        end = sensor.sensor.x + range
-
-        if self.start == None and self.end == None:
-            self.start = start
-            self.start = end
-
-        if start < self.end and end > self.start:
-            if start < self.start:
-                self.start = start
-            if end > self.end:
-                self.end = end
-            return
-        
-        if end < self.start:
-            if self.left_node != None:
-                self.left_node.add_sensor(sensor, y_pos)
-            else:
-                self.left_node = Interval(start, end)
-            return
-
-        if start > self.end:
-            if self.left_node != None:
-                self.left_node.add_sensor(sensor, y_pos)
-            else:
-                self.left_node = Interval(start, end)
-            return
-        
-    def check_children(self, parent):
-        if self.left_node != None:
-            self.left_node.check_children(parent)
-            self.left_node = None
-        if self.right_node != None:
-            curr_node = self.right_node
-            self.right_node = None
-            curr_node.check_children(parent)
-            
-        parent.add_sensor(self.start)
-
+def get_end(range):
+    return range[1]
 
 if __name__ == "__main__":
     main()
