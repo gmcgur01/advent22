e4dd6c4d9fe7f2996a037cf321bfb2b39a17994d
gmcgur01 <George.McGurkin@tufts.edu>
2022-12-20 23:02:34 -0500

    rocks part 2

--- rocks/rocks.py
+++ rocks/rocks.py
@@ -1,38 +1,26 @@
 #! /usr/bin/env python3
 import sys
 from dataclasses import dataclass
-
+import math
 
 def main():
     if len(sys.argv) != 2:
         sys.exit(f"Usage: {sys.argv[0]} <file name>")
     try:
         with open(sys.argv[1]) as file:
-            cave = Cave(file.readline().strip())
+            jet_pattern = file.readline().strip()
     except FileNotFoundError:
         sys.exit(f"{sys.argv[1]}: Unable to open file")
 
-    rock_total = 0
-    round_num = 0
-    new_rock = True
-    while rock_total < 2022:
-        if new_rock:
-            curr_rock = cave.generate_rock(rock_total)
-            new_rock = False
-
-        if cave.get_jet(round_num) == "<":
-            curr_rock.move_left()
-        else:
-            curr_rock.move_right()
-
-        if not curr_rock.move_down():
-            curr_rock.draw_rock()
-            new_rock = True
-            rock_total += 1
-
-        round_num += 1
-   
-    print(cave.cave_height())
+    start, end = find_cycle(Cave(jet_pattern))
+    # how many rocks in each cycle
+    cycle_length = end[0] - start[0]
+    # get the offset height (height until the start of cycle + extra at the end)
+    offset = get_height(Cave(jet_pattern), ((1000000000000 - start[0]) % cycle_length) + start[0])
+    # calculate the number of cycles needed to reach 1 trillion
+    num_cycles = math.floor((1000000000000 - start[0]) / cycle_length)
+    
+    print(num_cycles * (end[1] - start[1]) + offset)
 
 class Cave:
     def __init__(self, jet_pattern):
@@ -80,6 +68,13 @@ class Cave:
             i -= 1
         return 0
 
+    def col_height(self, col_num):
+        tallest = self.cave_height()
+        i = tallest - 1
+        while i != -1 and (self.cave[i][col_num]) != 1:
+            i -= 1
+        return tallest - (i + 1)
+        
     def add_row(self, row):
         if len(row) != 7:
             raise ValueError("Cave row must be 7 elements long")
@@ -147,6 +142,52 @@ class Point:
     x: int
     y: int
 
+def find_cycle(cave):
+    # dict used to store states; if the same state occurs twice, we have a cycle
+    states = {}
+    rock_total = 0
+    round_num = 0
+    new_rock = True
+    while True:
+        if new_rock:
+            curr_rock = cave.generate_rock(rock_total)
+            new_rock = False
+        if cave.get_jet(round_num) == "<":
+            curr_rock.move_left()
+        else:
+            curr_rock.move_right()
+        if not curr_rock.move_down():
+            curr_rock.draw_rock()
+            new_rock = True
+            # state is the round_num (how far into the jet pattern we are), 
+            # the rock type, and the height difference for all 7 columns
+            curr_state = tuple([round_num, rock_total % 5, tuple([cave.col_height(i) for i in range(7)])])
+            if curr_state in states:
+                return states[curr_state], (rock_total, cave.cave_height())
+            else:
+                states[curr_state] = (rock_total, cave.cave_height())
+            rock_total += 1
+        round_num = ((round_num +  1) % cave.jet_len)
+
+def get_height(cave, rocks):
+    rock_total = 0
+    round_num = 0
+    new_rock = True
+    while rock_total < rocks:
+        if new_rock:
+            curr_rock = cave.generate_rock(rock_total)
+            new_rock = False
+        if cave.get_jet(round_num) == "<":
+            curr_rock.move_left()
+        else:
+            curr_rock.move_right()
+        if not curr_rock.move_down():
+            curr_rock.draw_rock()
+            new_rock = True
+            rock_total += 1
+        round_num = ((round_num +  1) % cave.jet_len)
+
+    return cave.cave_height()
 
 if __name__ == "__main__":
     main()
